3ec79a4c271b1a4859d88d697e7cb05a5b7810bb
Stefano Tron <stefano.tron@supercam.co>
2026-03-03 08:18:33 +0000

    feat: export AddressDisplay, TransactionAmount, ConfirmationBadge

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-export { BTCAmount, BTCInput } from "./components";
-export type { BTCAmountProps, BTCInputProps } from "./components";
+export {
+  BTCAmount,
+  BTCInput,
+  AddressDisplay,
+  TransactionAmount,
+  ConfirmationBadge,
+} from "./components";
+export type {
+  BTCAmountProps,
+  BTCInputProps,
+  AddressDisplayProps,
+  TransactionAmountProps,
+  ConfirmationBadgeProps,
+} from "./components";
 export { BitcoinIcon, SatsIcon } from "./icons";
 export type { BitcoinIconProps, SatsIconProps } from "./icons";
